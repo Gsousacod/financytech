912084a012e14503992794d3a94cdc0a1aa3f8d7
Gabriel <gs5103809@gmail.com>
2025-03-19 18:22:33 -0300

    refactor: pageTitltUpdate refactor

--- components/PageTitleUpdate.tsx
+++ components/PageTitleUpdate.tsx
@@ -1,23 +1,13 @@
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
-
 
 const PageTitleUpdater = () => {
-    
     const pathname = usePathname();
-    
-        // Mapeamento de rotas para nomes de páginas
-        const pageNames: Record<string, string> = {
+    const pageNames: Record<string, string> = {
         "/dashboard": "Início",
         "/releases": "Lançamentos",
         "/settings": "Configurações",
         "/reports": "Relatórios",
         };
-
-        
-        
-    
-
     return document.title = pageNames[pathname]; // Componente auxiliar, não precisa renderizar nada
 };
 
